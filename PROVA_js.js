d9396fc8b0cd4a862b53f8ac357bafc02b06f454
Alessia438 <56439170+Alessia438@users.noreply.github.com>
2020-10-06 16:38:23 +0200

    Update PROVA_js.js

--- PROVA_js.js
+++ PROVA_js.js
@@ -112,16 +112,24 @@ function changeArticle(articleNum, issueNum){
 	var c = document.getElementById(issueNum).children;
 	for (var i=1; i<=3; i++) {
 		if ("article" + i === articleNum) {
-			c[i-1].style.display = "block";
+			c[i].style.display = "block";
 		}
 		else {
-			c[i-1].style.display = "none";
+			c[i].style.display = "none";
 		}
 	}
 }
 	
-function changeArticleCover{
-	
+function changeArticleCover(articleNum, issueNum) {
+	var c = window.parent.document.getElementById(issueNum).children;
+	for (var i=1; i<=3; i++) {
+		if ("article" + i === articleNum) {
+			c[i].style.display = "block";
+		}
+		else {
+			c[i].style.display = "none";
+		}
+	}
 }
 	
 
