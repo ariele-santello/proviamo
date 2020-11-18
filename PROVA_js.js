459d12a151650a6c123f2f2c359d68cba21eab1b
Alessia438 <56439170+Alessia438@users.noreply.github.com>
2020-11-18 12:21:18 +0100

    tolto dal commento pos (440-447)

--- PROVA_js.js
+++ PROVA_js.js
@@ -436,18 +436,17 @@ function createOccurrenceLi(occurrence, occurrenceValue, newUl, n, myFrames, myL
 
 	occurrenceLi.appendChild(instanceNode);
 
-	/*
+	
 	//numero di li il cui span o elemento time corrispondente ha lo stesso parent di quello corrente
 	var pos = 0;
 	for (var ulchild of newUl.children){
-		if occurrence.parent.id === ulchild.data-parent{ // controllare risultato di === False
+		if occurrence.parent.id === ulchild.getAttribute('data-parent'){
 			pos++;
 		}
 	}
 	occurrenceLi.setAttribute('data-parent', occurrence.parent.id);
-	*/
 
-	var citNode = document.createTextNode('" '+ parsing(occurrenceValue, occurrence.parentNode)+'"'); //vedi se fare textNode o innerHTML
+	var citNode = document.createTextNode('" '+ parsing(occurrenceValue, occurrence.parentNode, pos)+'"'); //vedi se fare textNode o innerHTML
 	occurrenceLi.appendChild(citNode); //appena tolto dal commento
 
 	var occurrenceId = occurrenceValue+"-"+(newUl.children.length+1);
