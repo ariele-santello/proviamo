55855b9bcc47d526e16969c356f1a29db33f60c3
Alessia438 <56439170+Alessia438@users.noreply.github.com>
2020-12-09 14:23:26 +0100

    typo

--- script/main.js
+++ script/main.js
@@ -539,7 +539,7 @@ function showMetaContent(){
 function findOrigin(){
 	if(window.location.href.includes('#article')){
 		if(document.getElementById("Origin").href.length <1){
-			getLinkOrigin((window.location.href.slice(window.location.href.indexOf('#')+1, window.location.href.length), document.getElementById("Origin"));
+			getLinkOrigin(window.location.href.slice(window.location.href.indexOf('#')+1, window.location.href.length), document.getElementById("Origin"));
 		}
 	}
 }
